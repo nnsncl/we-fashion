719531e62e7e74c1650be801034bd0bf36218076
nnsncl <nuniroland@gmail.com>
2021-05-25 20:58:31 +0200

    Update product controller : add discount method

--- we-fashion/app/Http/Controllers/ProductsController.php
+++ we-fashion/app/Http/Controllers/ProductsController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+
 class ProductsController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $products = Product::all();
         return view('index', [
             'products' => $products
         ]);
     }
+
+    public function discount(Product $discount_products) {
+        $discount_products = DB::table('products')
+            ->where('discount', '=', 1)
+            ->get();
+                
+        return view('discount.index', [
+            'discount_products' => $discount_products
+        ]);
+    }
+
 }
