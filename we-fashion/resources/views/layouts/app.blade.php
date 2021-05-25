2fa48e16da5780fdf654d65b1698df8396ffdd11
nnsncl <nuniroland@gmail.com>
2021-05-25 21:27:43 +0200

    create men view

--- we-fashion/resources/views/layouts/app.blade.php
+++ we-fashion/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         <a href="/" class="text-xl font-bold" >Wf.</a>
         <div class="flex items-center space-x-6 sm:space-x-10 ml-6 sm:ml-10" >
             <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2 bg-white p-5 rounded-3xl" href="/discount">Offers</a>
-            <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2" href="">Men</a>
+            <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2" href="/men">Men</a>
             <a class="text-base leading-6 font-medium hover:text-gray-600 transition-colors duration-200 py-2" href="">Women</a>
         </div>
     </nav>
