188bc85fd9c205c5efd86415814726131f4f6f1b
nnsncl <nuniroland@gmail.com>
2021-05-28 22:08:09 +0200

    Adjust product seeder

--- we-fashion/database/seeders/ProductsSeeder.php
+++ we-fashion/database/seeders/ProductsSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Product;
 use App\Models\Category;
+use App\Models\Size;
 
 class ProductsSeeder extends Seeder
 {
@@ -16,13 +17,16 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        Category::create([
-            "gender" => "women"
-        ]);
 
-        Category::create([
-            "gender" => "men"
-        ]);
+        $category_values = ["men", "women"];
+        foreach($category_values as $category) {
+            Category::create(["gender" => $category]);
+        }
+
+        $size_values = ["XS","S","M","L","XL"];
+        foreach($size_values as $size) {
+            Size::create(["value" => $size]);
+        }
 
         Product::factory()
             ->count(80)
@@ -30,11 +34,19 @@ class ProductsSeeder extends Seeder
             ->each(function($product) {
                 $category = Category::find(rand(1, 2));
                 $product->category()->associate($category);
+
+                $sizes = Size::pluck('id')
+                                ->shuffle()
+                                ->slice(0, rand(1, 5))
+                                ->all();
+
                 $files = Storage::allFiles($category->gender === 'women' ? 'public/images/women' : 'public/images/men');
                 $files_names = str_replace("public/", "", $files);
                 $fileindex = array_rand($files);
                 $file = $files_names[$fileindex];
+                
                 $product->image()->create([ 'link' => $file ]);
+                $product->sizes()->attach($sizes);
                 $product->save();
         });
     }
