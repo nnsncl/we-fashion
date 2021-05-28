ed22b4fa36278dfedf371e2cc847d3d217de71e9
nnsncl <nuniroland@gmail.com>
2021-05-28 10:55:46 +0200

    Rename front controller

--- we-fashion/app/Http/Controllers/FrontController.php
+++ we-fashion/app/Http/Controllers/FrontController.php
@@ -3,19 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
+use App\Models\Front;
 use Illuminate\Support\Facades\DB;
 
-class ProductsController extends Controller
+class FrontController extends Controller
 {
     public function index() {
-        $products = Product::paginate(6);
+        $products = Front::paginate(6);
         return view('front.index', [
             'products' => $products
         ]);
     }
 
-    public function discount(Product $discount_products) {
+    public function discount(Front $discount_products) {
         $discount_products = DB::table('products')
             ->where('discount', '=', 1)
             ->paginate(6);
@@ -26,14 +26,14 @@ class ProductsController extends Controller
     }
 
     public function details(int $id) {
-        $details = Product::find($id);
+        $details = Front::find($id);
         
         return view('front.details.index', [
             'details' => $details
         ]);
     }
 
-    public function womenProducts(Product $women_products)
+    public function womenProducts(Front $women_products)
     {
         $women_products = DB::table('products')
             ->where('category_id', '=', 1)
@@ -44,7 +44,7 @@ class ProductsController extends Controller
             ]);
     }
 
-    public function menProducts(Product $men_products)
+    public function menProducts(Front $men_products)
     {
         $men_products = DB::table('products')
             ->where('category_id', '=', 2)
--- we-fashion/routes/web.php
+++ we-fashion/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AdministrationController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 /*
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Auth;
 */
 
 // Guests Routes
-Route::resource('/', ProductsController::class);
+Route::resource('/', FrontController::class);
 
-Route::get('/discount', [ProductsController::class, 'discount']);
-Route::get('/details/{product}', [ProductsController::class, 'details'])->where(['id' => '[0-9]+']);
-Route::get('/men', [ProductsController::class, 'menProducts']);
-Route::get('/women', [ProductsController::class, 'womenProducts']);
+Route::get('/discount', [FrontController::class, 'discount']);
+Route::get('/details/{product}', [FrontController::class, 'details'])->where(['id' => '[0-9]+']);
+Route::get('/men', [FrontController::class, 'menProducts']);
+Route::get('/women', [FrontController::class, 'womenProducts']);
 
 // Admins Routes
 Auth::routes();
