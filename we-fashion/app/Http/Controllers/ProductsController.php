257ef22e997c3a2609e1ef8e137539f2f5cb58a6
nnsncl <nuniroland@gmail.com>
2021-05-29 12:05:36 +0200

    Adjust controller to access categories vlues

--- we-fashion/app/Http/Controllers/ProductsController.php
+++ we-fashion/app/Http/Controllers/ProductsController.php
@@ -4,49 +4,60 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
     public function index() {
         $products = Product::paginate(6);
+        $categories = Category::pluck('gender', 'id')->all();
 
         return view('front.index', [
-            'products' => $products
+            'products' => $products,
+            'categories' => $categories,
         ]);
     } 
 
     public function discount(Product $discount_products) {
         $discount_products = Product::where('discount', '=', 1)->paginate(6);
+        $categories = Category::pluck('gender', 'id')->all();
 
         return view('front.discount.index', [
-            'discount_products' => $discount_products
+            'discount_products' => $discount_products,
+            'categories' => $categories,
         ]);
     }
 
     public function details(int $id) {
         $details = Product::find($id);
-        
+        $categories = Category::pluck('gender', 'id')->all();
+
         return view('front.details.index', [
-            'details' => $details
+            'details' => $details,
+            'categories' => $categories,
         ]);
     }
 
     public function womenProducts(Product $women_products)
     {
         $women_products = Product::where('category_id', '=', 1)->paginate(6);
+        $categories = Category::pluck('gender', 'id')->all();
 
         return view('front.categories.women.index', [
-                'women_products' => $women_products
-            ]);
+            'women_products' => $women_products,
+            'categories' => $categories,
+        ]);
     }
 
     public function menProducts(Product $men_products)
     {
         $men_products = Product::where('category_id', '=', 2)->paginate(6);
+        $categories = Category::pluck('gender', 'id')->all();
 
         return view('front.categories.men.index', [
-                'men_products' => $men_products
-            ]);
+            'men_products' => $men_products,
+            'categories' => $categories,
+        ]);
     }
 }
